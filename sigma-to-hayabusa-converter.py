37977eb2c447b2753e5970a4a700a4c9d84d22a3
fukusuket <41001169+fukusuket@users.noreply.github.com>
2025-09-16 12:46:34 +0900

    chg: remove unused function

--- sigma-to-hayabusa-converter.py
+++ sigma-to-hayabusa-converter.py
@@ -120,14 +120,6 @@ def convert_special_val(key: str, value: str | list[str]) -> str | list[str]:
     return value
 
 
-def convert_date_format(obj: dict) -> dict:
-    date_fields = ["date", "modified"]
-    for field in date_fields:
-        if field in obj and isinstance(obj[field], str):
-            if re.match(r'^\d{4}/\d{2}/\d{2}$', obj[field]):
-                obj[field] = obj[field].replace('/', '-')
-    return obj
-
 def assign_uuid_for_convert_rules(obj: dict, logsource_hash: str) -> dict:
     if "id" not in obj:
         return dict(obj)
